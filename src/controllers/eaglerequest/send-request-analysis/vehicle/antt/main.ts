ee0c68d861f7a6a89ab4f9ae66d43f0359bc6ea1
Kazu-Kadota <kadota.kazuyuki@gmail.com>
2024-11-08 00:11:20 -0300

    feat(controller): add send request analysis vehicle antt

---
+++ src/controllers/eaglerequest/send-request-analysis/vehicle/antt/main.ts
@@ -0,0 +1,69 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
+import { SNSClient } from '@aws-sdk/client-sns'
+import { AnalysisTypeEnum } from 'src/models/dynamo/request-enum'
+import { Controller } from 'src/models/lambda'
+import useCasePublishSnsTopicVehicle from 'src/use-cases/publish-techimze-sns-topic-vehicle'
+import ErrorHandler from 'src/utils/error-handler'
+import { UserInfoFromJwt } from 'src/utils/extract-jwt-lambda'
+import logger from 'src/utils/logger'
+import removeEmpty from 'src/utils/remove-empty'
+
+import validateBodyVehicleANTT from './validate-body-vehicle'
+import vehicleANTTAnalysis, { VehicleAnalysisRequest } from './vehicle-antt'
+
+const dynamodbClient = new DynamoDBClient({ region: 'us-east-1' })
+
+const snsClient = new SNSClient({
+  region: 'us-east-1',
+  maxAttempts: 5,
+})
+
+const requestAnalysisVehicleANTT: Controller = async (req) => {
+  const user_info = req.user_info as UserInfoFromJwt
+  const event_body = removeEmpty(JSON.parse(req.body as string))
+
+  const body = validateBodyVehicleANTT(event_body)
+
+  if (user_info.user_type === 'admin' && !body.company_name) {
+    logger.warn({
+      message: 'Need to inform company name for admin user',
+      user_type: user_info.user_type,
+    })
+
+    throw new ErrorHandler('É necessário informar o nome da empresa para usuários admin', 400)
+  }
+
+  const vehicle_analysis_constructor: VehicleAnalysisRequest = {
+    analysis_type: AnalysisTypeEnum.VEHICLE,
+    body,
+    dynamodbClient,
+    user_info,
+  }
+
+  const vehicle_analysis = await vehicleANTTAnalysis(vehicle_analysis_constructor)
+
+  await useCasePublishSnsTopicVehicle({
+    owner_document: body.owner_document,
+    plate: vehicle_analysis.plate,
+    request_id: vehicle_analysis.request_id,
+    snsClient,
+    vehicle_analysis_type: vehicle_analysis.vehicle_analysis_type,
+    vehicle_id: vehicle_analysis.vehicle_id,
+  })
+
+  logger.info({
+    message: 'Successfully requested to analyze vehicle antt',
+    request_id: vehicle_analysis.request_id,
+    vehicle_id: vehicle_analysis.vehicle_id,
+    plate: body.plate,
+  })
+
+  return {
+    body: {
+      message: 'Successfully requested to analyze vehicle antt',
+      ...vehicle_analysis,
+    },
+  }
+}
+
+export default requestAnalysisVehicleANTT
